ade04beb88d0dce6b83fc6b8a900902b65d7a42f
AliFarajzade <alifarajzade1@yahoo.com>
2022-03-15 06:27:04 -0400

    Code splitting added.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,40 +1,47 @@
 import { Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+import Loader from '../components/loader.component'
 
-import ProfilePage from '../pages/profile.page'
-import ExplorePage from '../pages/explore.page'
-import SignInPage from '../pages/sign-in.page'
-import SignUpPage from '../pages/sign-up.page'
-import OffersPage from '../pages/offers.page'
-import ForgotPasswordPage from '../pages/forgot-password.page'
-import NotFoundPage from '../pages/not-found.page'
-import CategoryPage from '../pages/category.page'
-import CreateListingPage from '../pages/create-listing.page'
-import Listing from '../pages/lisitng.page'
-import ContactPage from '../pages/contact.page'
-import EditListingPage from '../pages/edit-listing.page'
+const ProfilePage = lazy(() => import('../pages/profile.page'))
+const ExplorePage = lazy(() => import('../pages/explore.page'))
+const SignInPage = lazy(() => import('../pages/sign-in.page'))
+const SignUpPage = lazy(() => import('../pages/sign-up.page'))
+const OffersPage = lazy(() => import('../pages/offers.page'))
+const ForgotPasswordPage = lazy(() => import('../pages/forgot-password.page'))
+const NotFoundPage = lazy(() => import('../pages/not-found.page'))
+const CategoryPage = lazy(() => import('../pages/category.page'))
+const CreateListingPage = lazy(() => import('../pages/create-listing.page'))
+const Listing = lazy(() => import('../pages/lisitng.page'))
+const ContactPage = lazy(() => import('../pages/contact.page'))
+const EditListingPage = lazy(() => import('../pages/edit-listing.page'))
 
 const RoutesManager = () => {
     return (
-        <Routes>
-            <Route path="/" element={<ExplorePage />} />
-            <Route path="/offers" element={<OffersPage />} />
-            <Route path="/login" element={<SignInPage />} />
-            <Route path="/register" element={<SignUpPage />} />
-            <Route path="/reset" element={<ForgotPasswordPage />} />
-            <Route path="/category/:categoryName" element={<CategoryPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/create-listing" element={<CreateListingPage />} />
-            <Route path="/contact/:landlordID" element={<ContactPage />} />
-            <Route
-                path="/edit-listing/:listingID"
-                element={<EditListingPage />}
-            />
-            <Route
-                path="/category/:categoryName/:houseID"
-                element={<Listing />}
-            />
-            <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
+        <Suspense fallback={<Loader loadingState={true} />}>
+            <Routes>
+                <Route path="/" element={<ExplorePage />} />
+                <Route path="/offers" element={<OffersPage />} />
+                <Route path="/login" element={<SignInPage />} />
+                <Route path="/register" element={<SignUpPage />} />
+                <Route path="/reset" element={<ForgotPasswordPage />} />
+                <Route
+                    path="/category/:categoryName"
+                    element={<CategoryPage />}
+                />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/create-listing" element={<CreateListingPage />} />
+                <Route path="/contact/:landlordID" element={<ContactPage />} />
+                <Route
+                    path="/edit-listing/:listingID"
+                    element={<EditListingPage />}
+                />
+                <Route
+                    path="/category/:categoryName/:houseID"
+                    element={<Listing />}
+                />
+                <Route path="/*" element={<NotFoundPage />} />
+            </Routes>
+        </Suspense>
     )
 }
 
